69ff2a2c1890cbddf14c37e57f011c3e41ce11d2
yulei <>
2022-03-26 10:23:47 +0800

    集成 cache 和mysql 等依赖

--- redis_study/demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ redis_study/demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -2,8 +2,10 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Demo1Application {
 
     public static void main(String[] args) {
