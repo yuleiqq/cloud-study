6993d6c99f9bc26614c75d42dddbcb6d553e6d00
yulei <>
2022-04-07 10:13:37 +0800

    工作队列demo实现

---
+++ rabbitmq/src/main/java/com/example/rabbitmq/work/fair/Send.java
@@ -0,0 +1,54 @@
+package com.example.rabbitmq.work.fair;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * 公平策略
+ */
+public class Send {
+
+
+    private final static  String QUEUE_NAME = "work_qq_fair";
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectionFactory  factory = new ConnectionFactory();
+
+        factory.setHost("8.140.98.27");
+        factory.setUsername("admin");
+        factory.setPassword("password");
+        factory.setVirtualHost("/");
+        factory.setPort(5672);
+
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+
+            /**
+             * 队列名称
+             * 持久化配置:mq重启后还在
+             * 是否独占:只能有一个消费者监听队列;当
+               connection关闭是否删除队列，一般是false，发布订阅是独 占
+             * 自动删除: 当没有消费者的时候，自动删除 掉，一般是false
+             * 其他参数 *
+             * 队列不存在则会自动创建，如果存在则不会
+               覆盖，所以此时的时候需要注意属性
+             */
+            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+
+            //轮训发送10个
+            for(int i=0; i<10; i++){
+                String message = "Hello Message "+i;
+                channel.basicPublish("",QUEUE_NAME,null,message.getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Send "+ message );
+            }
+
+        }
+
+    }
+
+}
---
+++ rabbitmq/src/main/java/com/example/rabbitmq/work/rr/Recv1.java
@@ -0,0 +1,42 @@
+package com.example.rabbitmq.work.rr;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+
+public class Recv1 {
+    private final static String QUEUE_NAME = "work_qq_rr";
+
+    public static void main(String[] argv)
+            throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("8.140.98.27");
+        factory.setUsername("admin");
+        factory.setPassword("password");
+        factory.setVirtualHost("/");
+        factory.setPort(5672);
+       //消费者一般不增加自动关闭
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        //队列的声明
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages.To exit press CTRL + C");
+        //回调方法，下面两种都行
+        Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                  // consumerTag 是固定的 可以做此会 话的名字， deliveryTag 每次接收消息+1
+                    System.out.println("consumerTag 消息标识=" + consumerTag);
+                    //可以获取交换机，路由健等
+                    System.out.println("envelope元数 据 = "+envelope);
+                    System.out.println(" properties 配置信息=" + properties);
+                    System.out.println("body=" + new String(body, "utf-8"));
+                    //手工确认消息，不是多条确认
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+
+                }
+    };
+        //关闭消息自动确认
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+        }
+    }
