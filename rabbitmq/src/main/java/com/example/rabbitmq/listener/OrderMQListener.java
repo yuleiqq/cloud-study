7a7aa56227e75ed6e22a4cf7a48533d1f0195556
yulei <>
2022-04-08 13:55:44 +0800

    消费者消费消息再次确认机制测试

--- rabbitmq/src/main/java/com/example/rabbitmq/listener/OrderMQListener.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/listener/OrderMQListener.java
@@ -1,22 +1,30 @@
 package com.example.rabbitmq.listener;
 
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @RabbitListener(queues = "order_queue")
 public class OrderMQListener {
 
     @RabbitHandler
-    public void messageHandler(String body, Message message){
+    public void messageHandler(String body, Message message, Channel channel) throws IOException {
 
         long msgTag = message.getMessageProperties().getDeliveryTag();
         System.out.println("msgTag = "+ msgTag);
         System.out.println("message = "+ message.toString());
         System.out.println("body = "+ body);
+
+        //告诉broker ，消息已经被确认
+        channel.basicAck(msgTag,true);
+        //告诉broker，消息拒绝确认，重新入队列
+        // channel.basicNack(msgTag,false,true);
 
     }
 
--- rabbitmq/src/main/java/com/example/rabbitmq/simple/Recv.java
+++ rabbitmq/src/main/java/com/example/rabbitmq/simple/Recv.java
@@ -5,8 +5,8 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 
 public class Recv {
-    private final static String QUEUE_NAME = "hello";
 
+    private final static String QUEUE_NAME = "hello";
     public static void main(String[] argv)
             throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -25,7 +25,6 @@ public class Recv {
         Consumer consumer = new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-
                   // consumerTag 是固定的 可以做此会 话的名字， deliveryTag 每次接收消息+1
                     System.out.println("consumerTag 消息标识=" + consumerTag);
                     //可以获取交换机，路由健等
