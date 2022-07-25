b69060bbd5640c00eb4635e9dd303212c9aa2666
yulei <yul41@chinaunicom.cn>
2022-07-25 10:07:15 +0800

    Flink 滚动窗口

--- flink-demo/src/main/java/com/example/flinkdemo/my/WindowWordCount.java
+++ flink-demo/src/main/java/com/example/flinkdemo/my/WindowWordCount.java
@@ -1,8 +1,49 @@
-package com.example.flinkdemo.my;/**
-*
-* @author: yulei
-* @create: 2022-07-25
-* @Version 1.0
-**/
+package com.example.flinkdemo.my;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ *
+ * 单词计数
+ *
+ *
+ * @author: yulei
+ * @create: 2022-07-25
+ * @Version 1.0
+ **/
 public class WindowWordCount {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env.socketTextStream("", 9999)
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    //对一行数据进行切割x
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+                        for (String word : value.split(" ")) {
+                            out.collect(new Tuple2<String, Integer>(word, 1));
+                        }
+                    }
+                }).keyBy(value -> value.f0)
+                //滚动窗口
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .sum(1);
+
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+
+    }
+
 }
